48c4a7cdaefc7c1f7c50b2f11e9df56317a7eea1
t3qvi1a <xinyan.ge.edu@gmail.com>
2026-07-17 11:59:54 +0800

    progress: finish ch03

---
+++ part01_basics/ch03_introducing_lists/lists.py
@@ -0,0 +1,54 @@
+# fruits = ['apple', 'banana', 'pineapple']
+# message = f"My favorite fruit is {fruits[0].title()}."
+#
+# print(fruits)
+# fruits[1] = "watermelon"
+# print(fruits)
+# fruits.append("mango")
+# print(fruits)
+#
+# print(fruits[2].upper())
+# print(message)
+
+
+# append
+fruits = []
+fruits.append("apple")
+fruits.append("apple")
+fruits.append("banana")
+fruits.append("mango")
+print(fruits)
+
+# insert
+fruits.insert(-1, "watermelon")
+print(fruits)
+
+# delete: not get the value
+del fruits[0]
+print(fruits)
+
+# pop: get the value
+poped_fruit = fruits.pop(0)
+print(f"poped fruit is: {poped_fruit}")
+print(f"the remaining fruits are: {fruits}")
+
+# remove: use the value to search
+fruits.remove('mango')
+print(fruits)
+
+# sort
+fruits.append("purple")
+fruits.append("orange")
+print(fruits)
+print(sorted(fruits))
+
+fruits.sort()
+print(fruits)
+
+fruits.sort(reverse = True)
+print(fruits)
+
+fruits.reverse()
+print(fruits)
+
+print(len(fruits))
