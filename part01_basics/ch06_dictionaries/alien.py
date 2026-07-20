fa44416686c7d1b86b8e924fac477b6c3c615c13
t3qvi1a <xinyan.ge.edu@gmail.com>
2026-07-20 11:54:39 +0800

    progress: start ch06

---
+++ part01_basics/ch06_dictionaries/alien.py
@@ -0,0 +1,8 @@
+alien_0 = {'color': 'green', 'points': 5}
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0['color'])
+alien_0['color'] = 'yellow'
+print(alien_0['color'])
+del alien_0['color']
